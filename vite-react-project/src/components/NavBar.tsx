6802e21e8e03decb13927d99d102db3d107e7148
Megan <meganthielman@gmail.com>
2023-02-16 13:46:32 -0800

    Navbar Mobile additions

--- vite-react-project/src/components/NavBar.tsx
+++ vite-react-project/src/components/NavBar.tsx
@@ -6,38 +6,38 @@ import { SidebarData } from './SidebarData'
 import { IconContext } from 'react-icons'
 
 export const NavBar = () => {
-  const [sidebar, setSidebar] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar)
 
   return (
-    <>
-    <IconContext.Provider value={{color: "#234e59"}}>
-      <div className="navbar">
-        <Link to="#" className="menu-bars">
-        <FaIcons.FaBars onClick={showSidebar} />
-        </Link>
-        </div>  
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items">
-            <li className="navbar-toggle">
-              <Link to="#" className="menu-bars">
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
-            {SidebarData.map((item, index)=> {
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div className='font-bold text-2xl cursor-point flex items-center text-gray-800'>
+          <span className='text-3xl text-indigo-600 mr-1 pt-2'>
+            <FaIcons.FaPaintRoller/>
+          </span>
+          Designer
+        </div>
+
+        <div onClick={()=> setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+            {open ? <AiIcons.AiOutlineClose /> : <FaIcons.FaBars />}
+        </div>
+
+        <ul className={open ? 'md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto  md:pl-0 pl-9 transition-all duration-500 ease-in top-20 opacity-100 md:opacity-100' : 'md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto  md:pl-0 pl-9 transition-all duration-500 ease-in top-[-490px]'}>
+        {SidebarData.map((item, index)=> {
               return (
-                <li key={index} className={item.classN}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
+                <li key={index} className="md:ml-8 text-xl md:my-0 my-7">
+                <Link to={item.path} className="text-green-500 hover:text-blue-800 duration-500">
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
               )
             })}
-          </ul>
-        </nav>
-        </IconContext.Provider>
-    </>
+            </ul>
+      </div>
+
+    </div>
   )
 }
--- vite-react-project/tailwind.config.cjs
+++ vite-react-project/tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Configure your color palette here
+        transparent: "transparent",
+        current: "currentColor",
+        wheat: "#f7cfbe",
+        cotton: "#c3bbb6",
+        gull: "#9ab0be",
+        summer: "#9abda9",
+        dianne: "#234e59",
+        dune: "#302c28",
+        everglade: "#1f3d2e",
+      },
+    },
   },
   plugins: [],
 };
