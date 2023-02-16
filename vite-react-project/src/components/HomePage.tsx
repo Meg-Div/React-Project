c452af29921e47210cb9561e84d8f8eaf45f5ed0
Megan <meganthielman@gmail.com>
2023-02-16 07:33:06 -0800

    Project update

--- vite-react-project/src/components/HomePage.tsx
+++ vite-react-project/src/components/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { NavBar } from './NavBar'
 
 export const HomePage = () => {
   return (
     <Link to={"/"}>
-    <div>HomePage</div>
+    <div>
+        <NavBar />
+        HomePage</div>
     </Link>
   )
 }
