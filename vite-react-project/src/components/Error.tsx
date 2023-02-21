76b9d63fd8860b86629e63ef014b14e78c9178b7
Megan <meganthielman@gmail.com>
2023-02-21 13:45:25 -0800

    Error Page updates

--- vite-react-project/src/components/Error.tsx
+++ vite-react-project/src/components/Error.tsx
@@ -2,10 +2,31 @@ import { Link } from "react-router-dom";
 
 export function Error(): JSX.Element {
   return (
-    <div>
-      <h1>Oops! You seem to be lost.</h1>
-      <p>Here are some helpful links:</p>
-      <Link to="/">Home</Link>
+    <div className="md:bg-gradient-to-r from-summer to-wheat">
+      <div className="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center content-center">
+        <div className="bg-white shadow overflow-hidden pb-10 flex flex-col items-center justify-center content-center	">
+          <div className="text-center pt-8 ">
+            <h1
+              id="noto"
+              className="text-9xl font-bold text-summer text-center"
+            >
+              404
+            </h1>
+          </div>
+          <h1 id="noto" className="text-6xl font-medium py-8 text-center">
+            Page not found
+          </h1>
+
+          <p id="noto" className="text-2xl pb-8 px-12 font-medium text-center">
+            The page you are looking for does not exist.
+          </p>
+          <Link to={`/`}>
+            <button className="bg-summer hover:gull text-white font-bold py-2 px-4 border border-gull rounded">
+              Home
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
